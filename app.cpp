444e49f77b3a5c8705d4b098bf13e7ca46829d45
Abdulmajeed Nomman <abdulmajeednomman@users.noreply.github.com>
2024-11-09 20:35:45 +0300

    Inital commit: Added README and main script

---
+++ app.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <AI/ai>
+
+class AI{};
+class MachineLearnin:AI{};
+class DeepLearning:AI{};
+
+int main()
+{
+AI ai;
+// Run AI Simualtion Application:
+ai.RunAPP();
+
+return 0;
+}
