fe58c535ecf2520d75d96cd247ada8298e1af087
Abdulmajeed Nomman <m3nt@hotmail.com>
2024-11-10 02:57:54 +0300

    Update build.sh
    add python build

--- build.sh
+++ build.sh
@@ -6,3 +6,21 @@ clang++ ai_simulator.cpp -o ai_simulator -std=c++11
 clang++ ai_simulator.cpp -o ai_simulator -std=c++11 `pkg-config --cflags --libs opencv4`
 #run:
 ./ai_simulator
+
+# python:
+pip install -r requirements.txt
+
+# or .venv:
+python3 -m venv .venv
+source .venv/bin/activate
+
+python -m pip show tensorflow tensorflow-hub
+# tensorflow 2.18.0
+pip install tensorflow
+# tensorflow-hub 0.16.1
+pip install tensorflow-hub
+
+# on macOS: conda or miniconad
+brew install miniforge
+conda create -n tf_env tensorflow
+conda activate tf_env
