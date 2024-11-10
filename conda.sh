e59a659fb4a4b8a21f6264e68522e64c5ef5cbe4
Abdulmajeed Nomman <abdulmajeednomman@users.noreply.github.com>
2024-11-10 05:43:48 +0300

    "main"

---
+++ conda.sh
@@ -0,0 +1,13 @@
+brew install --cask miniconda
+
+# install tensorflow:
+conda create -n tf_env tensorflow
+
+
+# To activate this environment, use                                             
+#                                                                               
+    $ conda activate tf_env                                                   
+#                                                                               
+# To deactivate an active environment, use                                      
+#                                                                               
+#     $ conda deactivate   
